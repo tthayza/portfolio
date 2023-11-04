e19cc422e01d794d9bef0373e2ea675f5aaf61a7
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-11-04 03:07:22 -0300

    :sparkles: feat: Add footer and apply icons with lucide-lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Github, Linkedin, LucideAngularModule } from 'lucide-angular';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, MainComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HeaderComponent, MainComponent, FooterComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    LucideAngularModule.pick({ Github, Linkedin }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
